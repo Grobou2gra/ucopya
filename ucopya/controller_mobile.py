ef8568e6259aa42250eab2b3d0b272c1a94049bf
Benoît Taine <ork@olol.eu>
2014-11-06 11:42:59 +0100

    Adding logon operation to the controller

--- ucopya/controller_mobile.py
+++ ucopya/controller_mobile.py
@@ -13,3 +13,19 @@ class ControllerMobile(object):
         self.session  = requests.Session()
         self.logged   = False
 
+    def do_login(self):
+        """Log the user to the captive portal"""
+
+        credentials = {
+            'action'        : 'authenticate',
+            'login'         : self.login,
+            'password'      : self.password,
+            'policy_accept' : 'false'
+        }
+
+        req = self.session.post(self.__PORTAL_API, credentials).json()
+
+        if not 'error' in req:
+            self.logged = True
+            self.digest = req['user']['passwordDigest']['value']
+            self.ssid   = req['user']['incomingZone']['value']
